cbcb0fdda73993e29d665be55084342c5d1b166a
ahmetemincopur <64459050+ahmetemincopur@users.noreply.github.com>
2024-04-18 19:41:24 +0300

    Mimari yapı oluşturuldu. Servisler, Sınıflar, Arayüzler oluşturulmaya başlandı.

---
+++ Core/MeetingManagementSystem.Domain/Entities/Meetings.cs
@@ -0,0 +1,18 @@
+﻿using MeetingManagementSystem.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetingManagementSystem.Domain.Entities
+{
+    public class Meetings : BaseEntity
+    {
+        public required string Name { get; set; }
+        public required DateTime StartDate { get; set; }
+        public required DateTime EndDate { get; set; }
+        public required string Explanation { get; set; }
+        public byte[]? UploadedDocument { get; set; }
+    }
+}
---
+++ Core/MeetingManagementSystem.Domain/Entities/Users.cs
@@ -0,0 +1,19 @@
+﻿using MeetingManagementSystem.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetingManagementSystem.Domain.Entities
+{
+    public class Users : BaseEntity
+    {
+        public required string Firstname { get; set; }
+        public required string Lastname { get; set; }
+        public required string Email { get; set; }
+        public required string PhoneNumber { get; set; }
+        public required string Password { get; set; }
+        public byte[]? ProfilePhoto { get; set; } 
+    }
+}
